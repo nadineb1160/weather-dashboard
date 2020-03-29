994935e6f57edda87bd80ed3c454599dd4adf905
nadineb <nadineb@umich.edu>
2020-03-28 21:12:39 -0700

    add functions and outline ajax

--- script.js
+++ script.js
@@ -1,25 +1,109 @@
-// Submit button
-// Grab input from search form
-// Possibly auto fill api?
+// Once document is loaded
+$(document).ready(function () {
 
-// URL open weather + city query
-// Add city name to list
+    // Current date formatted 
+    let currentDay = moment().format("MMMM Do YYYY");
 
-// Display today's forcast
-// Name
-// Date
-// Icon
-// Temperature
-// Wind speed
-// UV index
+    console.log(currentDay);
 
-// Display 5-day forcast
-// Date
-// Icon
-// Temperature
-// Humidity
+    // Set current date 
+    // $("#currentDay").text(currentDay);
 
-// Display UV color 
+    // Saved cities
+    var savedcitiesArr = [];
 
-// Save city list to localStorage
+    var city = "";
+    var name, temperature, wind, humdity, uvIndex, icon;
+
+    // Submit button
+    $(".submit").on("click", function (event) {
+        event.preventDefault();
+
+        // Possibly auto fill api?
+
+        // City input
+        city = $("#city").val();
+        // console.log(city);
+
+        // Push new city to saved array
+        savedcitiesArr.push(city);
+
+        // Get data
+        getData();
+
+        // Display new city
+        displayCity();
+
+        // Display 5-day forcast
+        displayFiveDay();
+
+    });
+
+    function getData() {
+        // var city = London, uk
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=d953636a06fd6af8b2c881b86b574429";
+        console.log(queryURL);
+        // URL open weather + city query
+        // Add city name to list
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response);
+            name = response.name;
+            temperature = response.main.temp;
+            icon = response.weather.icon;
+            humdity = response.main.humidity;
+            wind = response.wind.speed;
+
+        })
+    }
+
+
+    // Display the current day dashboard of city
+    function displayCity() {
+        console.log(city);
+        // Display today's forcast
+        // Name
+        $(".city-name").text(name);
+        // Date
+        $(".current-date").text(currentDay);
+        // Icon
+        $(".current-icon").text(icon);
+        // Temperature
+        $(".temperature").text(temperature);
+        // Wind speed
+        $(".wind").text(wind);
+        // UV index
+        $(".uv").text("UV");
+
+        // Display UV color
+        displayUV();
+    }
+
+    // Display UV color 
+    function displayUV() {
+        console.log("UV");
+    }
+
+
+    // Display 5-day forcast
+    function displayFiveDay() {
+
+        // Date
+        // Icon
+        // Temperature
+        // Humidity
+    }
+
+    // Click on city buttons
+    // $(document).on("click", ".city-btn", function() {
+
+    // })
+
+
+    // Save city list to localStorage
+});
+
+
 
