07eff16adbab85c11ec52375c9b1380bf34cf629
nadineb <nadineb@umich.edu>
2020-03-28 19:39:10 -0700

    add pseudo code

---
+++ script.js
@@ -0,0 +1,25 @@
+// Submit button
+// Grab input from search form
+// Possibly auto fill api?
+
+// URL open weather + city query
+// Add city name to list
+
+// Display today's forcast
+// Name
+// Date
+// Icon
+// Temperature
+// Wind speed
+// UV index
+
+// Display 5-day forcast
+// Date
+// Icon
+// Temperature
+// Humidity
+
+// Display UV color 
+
+// Save city list to localStorage
+
