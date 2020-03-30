47a1d199e631ffcd2a68b1f2951d81ba7c0cebae
nadineb <nadineb@umich.edu>
2020-03-29 18:28:44 -0700

    add icon URL, change order of display data

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
+
+
 // Once document is loaded
 $(document).ready(function () {
 
     // Current date formatted 
     let currentDay = moment().format("MMMM Do YYYY");
 
-    console.log(currentDay);
-
-    // Set current date 
-    // $("#currentDay").text(currentDay);
+    // console.log(currentDay);
 
     // Saved cities
     var savedcitiesArr = [];
 
     var city = "";
-    var name, temperature, wind, humdity, uvIndex, icon;
+    var name, temperature, wind, humdity, uvIndex, iconImg;
 
     // Submit button
     $(".submit").on("click", function (event) {
+        // Prevent default form behavior
         event.preventDefault();
 
         // Possibly auto fill api?
@@ -31,30 +31,34 @@ $(document).ready(function () {
         // Get data
         getData();
 
-        // Display new city
-        displayCity();
-
-        // Display 5-day forcast
-        displayFiveDay();
+        // Save data
 
     });
 
     function getData() {
+        console.log("Get Data");
         // var city = London, uk
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=d953636a06fd6af8b2c881b86b574429";
-        console.log(queryURL);
+        // console.log(queryURL);
         // URL open weather + city query
         // Add city name to list
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // console.log(response);
+            console.log(response);
             name = response.name;
             temperature = response.main.temp;
-            icon = response.weather.icon;
+            iconImg = response.weather[0].icon;
+            // console.log("Icon " + iconImg);
             humdity = response.main.humidity;
             wind = response.wind.speed;
+
+            // Display new city
+            displayCity();
+
+            // Display 5-day forcast
+            displayFiveDay();
 
         })
     }
@@ -62,6 +66,7 @@ $(document).ready(function () {
 
     // Display the current day dashboard of city
     function displayCity() {
+        console.log("Display city");
         console.log(city);
         // Display today's forcast
         // Name
@@ -69,7 +74,10 @@ $(document).ready(function () {
         // Date
         $(".current-date").text(currentDay);
         // Icon
-        $(".current-icon").text(icon);
+        var iconURL = "http://openweathermap.org/img/wn/" + iconImg + "@2x.png";
+        console.log(iconImg);
+        console.log(iconURL);
+        $(".current-icon").attr("src", iconURL);
         // Temperature
         $(".temperature").text(temperature);
         // Wind speed
